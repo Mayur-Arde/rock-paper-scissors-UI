31ab4f63ff26db8a7f09c6696a8f4b693ca72ebf
Mayur-Arde <mayurarde8@gmail.com>
2021-09-22 18:28:09 +0530

    lizard and spock added

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,6 @@
 const buttons = document.querySelectorAll('.pick');
 const myScoreEl = document.getElementById('my-score');
 const computerScoreEl = document.getElementById('computer-score');
-const choices = ['paper', 'scissors', 'rock'];
 const main = document.getElementById('main');
 const selection = document.getElementById('selection');
 const resetBtn = document.getElementById('reset');
@@ -12,6 +11,7 @@ const openBtn = document.getElementById('open');
 const closeBtn = document.getElementById('close');
 const rulesModal = document.getElementById('modal');
 const blackBack = document.querySelector('.rules-container');
+const choices = ['paper', 'scissors', 'rock', 'lizard', 'spock'];
 
 //Initialize scores & user choice
 let myScore = 0;
@@ -20,7 +20,7 @@ let userChoice = undefined;
 
 //
 buttons.forEach(button => {
-  button.addEventListener('click', ()=>{
+  button.addEventListener('click', () => {
     userChoice = button.getAttribute('data-choice');
     main.style.display = 'none';
     selection.style.display = 'flex';
@@ -29,25 +29,25 @@ buttons.forEach(button => {
 });
 
 //Play again button
-resetBtn.addEventListener('click',()=>{
+resetBtn.addEventListener('click', () => {
   main.style.display = 'flex';
   selection.style.display = 'none';
 });
 
 //Show game's rules
-openBtn.addEventListener('click',()=>{
+openBtn.addEventListener('click', () => {
   rulesModal.style.display = 'flex';
-  blackBack.style.display='flex';
+  blackBack.style.display = 'flex';
 });
 
 //Hide game's rules
-closeBtn.addEventListener('click',()=>{
+closeBtn.addEventListener('click', () => {
   rulesModal.style.display = 'none';
-  blackBack.style.display='none';
+  blackBack.style.display = 'none';
 });
 
 // Function to check who wins
-function checkWinner(){
+function checkWinner() {
   // store computer choice
   const computerChoice = pickRandomChoice();
 
@@ -55,60 +55,61 @@ function checkWinner(){
   updateSelection(userSelect, userChoice);
   updateSelection(computerSelect, computerChoice);
 
-  if (userChoice === computerChoice){
+  if (userChoice === computerChoice) {
     //Draw
-    result.innerText= "draw";
+    result.innerText = "draw";
     result.style.color = 'white';
-  }else if(userChoice === 'paper' && computerChoice==='rock'
-  || userChoice === 'rock' && computerChoice === 'scissors'
-  || userChoice === 'scissors' && computerChoice === 'paper'){
+  } else if (userChoice === 'scissors' && computerChoice === 'paper' || userChoice === 'paper' && computerChoice === 'rock' || userChoice === 'rock' && computerChoice === 'lizard' || userChoice === 'lizard' && computerChoice === 'spock' || userChoice === 'spock' && computerChoice === 'scissors'|| userChoice === 'rock' && computerChoice === 'scissors' || userChoice === 'scissors' && computerChoice === 'lizard' || userChoice === 'lizard' && computerChoice === 'paper' || userChoice === 'paper' && computerChoice === 'spock' || userChoice === 'spock' && computerChoice === 'rock'
+   ) {
     //User won
     myScoreUpdate();
-    result.innerText= "win";
+    result.innerText = "win";
     result.style.color = 'green';
     // result.style.boxShadow = '0px 0px 5px black'
-  }else{
+  } else {
     //Computer won
-    result.innerText= "lost";
+    result.innerText = "lost";
     computerScoreUpdate();
     result.style.color = 'red';
   }
 }
 
 //Update user score
-function myScoreUpdate(){
+function myScoreUpdate() {
   myScore++;
   myScoreEl.innerText = myScore;
 }
 
 //Update computer score
-function computerScoreUpdate(){
+function computerScoreUpdate() {
   computerScore++;
   computerScoreEl.innerText = computerScore;
 }
 
 // Function to pick computer choice
-function pickRandomChoice(){
-  return choices[Math.floor(Math.random()*choices.length)];
+function pickRandomChoice() {
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
 //Update user selection
-function updateSelection(selectionEl, choice){
+function updateSelection(selectionEl, choice) {
   selectionEl.classList.remove('btn-paper');
   selectionEl.classList.remove('btn-scissors');
   selectionEl.classList.remove('btn-rock');
+  selectionEl.classList.remove('btn-lizard');
+  selectionEl.classList.remove('btn-spock');
   //update selection image
   const img = selectionEl.querySelector('img');
-  selectionEl.classList.add('btn-'+choice);
-  img.src='images/icon-'+choice+'.svg';
+  selectionEl.classList.add('btn-' + choice);
+  img.src = 'images/icon-' + choice + '.svg';
   img.alt = choice;
 }
 
 // ??////// restart???
 const restart = document.getElementById('btn-restart');
-restart.addEventListener('click',()=>{
- myScore = 0;
- computerScore = 0;
+restart.addEventListener('click', () => {
+  myScore = 0;
+  computerScore = 0;
   computerScoreEl.textContent = 0;
   myScoreEl.textContent = 0;
   main.style.display = 'flex';
